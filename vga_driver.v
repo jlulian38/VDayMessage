306326e65451ebb23fa25d3864595da4a54a07d8
Julian R. Moore <jreedmoore@gmail.com>
2014-02-14 11:16:01 -0500

    Adds all elements of "card" and corresponding data files

--- vga_driver.v
+++ vga_driver.v
@@ -40,7 +40,7 @@ module vga_driver(
 		.blank(blank)
 	);
 	
-	localparam MODULES = 4;
+	localparam MODULES = 7;
 	
 	wire [7:0] rgb_mux [0:MODULES-1];
 	
@@ -58,17 +58,110 @@ module vga_driver(
 	 
 	 //MODULES
 	 
-	string_display instance_name (
+	//I
+	string_display 
+		#(.OFFSET_X(8),
+		  .OFFSET_Y(8),
+		  .SCALE(2),
+	          .FILE("i.dat"),
+		  .LENGTH(1))
+	I_inst (
 		.hcnt(hcnt), 
 		.vcnt(vcnt), 
 		.enable(1'b1), 
 		.rgb(rgb_mux[0]), 
 		.active(rgb_sel[0])
 	);
+
+	//Heart (centered)
+	string_display 
+		#(.OFFSET_X(288),
+		  .OFFSET_Y(208),
+		  .SCALE(3),
+	          .FILE("heart.dat"),
+		  .LENGTH(1))
+	Heart_inst (
+		.hcnt(hcnt), 
+		.vcnt(vcnt), 
+		.enable(1'b1), 
+		.rgb(rgb_mux[1]), 
+		.active(rgb_sel[1])
+	);
 	
-	assign rgb_mux[1] = 8'b0;
-	assign rgb_mux[2] = 8'b0;
-	assign rgb_mux[3] = 8'b0;
+	//Katrina
+	string_display 
+		#(.OFFSET_X(272),
+		  .OFFSET_Y(400),
+		  .SCALE(2),
+	          .FILE("katrina.dat"),
+		  .LENGTH(1))
+	Katrina_inst (
+		.hcnt(hcnt), 
+		.vcnt(vcnt), 
+		.enable(1'b1), 
+		.rgb(rgb_mux[2]), 
+		.active(rgb_sel[2])
+	);
+
+	//line1
+	string_display 
+		#(.OFFSET_X(432),
+		  .OFFSET_Y(8),
+		  .SCALE(2),
+	          .FILE("line1.dat"),
+		  .LENGTH(1))
+	line1_inst (
+		.hcnt(hcnt), 
+		.vcnt(vcnt), 
+		.enable(1'b1), 
+		.rgb(rgb_mux[3]), 
+		.active(rgb_sel[3])
+	);
+
+	//line2
+	string_display 
+		#(.OFFSET_X(432),
+		  .OFFSET_Y(18),
+		  .SCALE(2),
+	          .FILE("line2.dat"),
+		  .LENGTH(1))
+	line2_inst (
+		.hcnt(hcnt), 
+		.vcnt(vcnt), 
+		.enable(1'b1), 
+		.rgb(rgb_mux[4]), 
+		.active(rgb_sel[4])
+	);
+
+	//line3
+	string_display 
+		#(.OFFSET_X(432),
+		  .OFFSET_Y(28),
+		  .SCALE(2),
+	          .FILE("line3.dat"),
+		  .LENGTH(1))
+	line3_inst (
+		.hcnt(hcnt), 
+		.vcnt(vcnt), 
+		.enable(1'b1), 
+		.rgb(rgb_mux[5]), 
+		.active(rgb_sel[5])
+	);
+
+	//line4
+	string_display 
+		#(.OFFSET_X(432),
+		  .OFFSET_Y(38),
+		  .SCALE(2),
+	          .FILE("line4.dat"),
+		  .LENGTH(1))
+	line1_inst (
+		.hcnt(hcnt), 
+		.vcnt(vcnt), 
+		.enable(1'b1), 
+		.rgb(rgb_mux[6]), 
+		.active(rgb_sel[6])
+	);
 	
 	function [31:0] log2;
 		input reg [31:0] value;
