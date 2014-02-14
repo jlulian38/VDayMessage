e8d5828407be7b3dc1e4f925082381a8fa49ad30
Julian R. Moore <jreedmoore@gmail.com>
2014-02-14 11:43:14 -0500

    Adds state machine to animate card

--- vga_driver.v
+++ vga_driver.v
@@ -46,6 +46,7 @@ module vga_driver(
 	
 	wire [MODULES-1:0] rgb_sel;
 	wire [log2(MODULES)-1:0] rgb_addr;
+	wire [MODULES-1:0] enable;
 	
 	assign rgb = (valid && !blank) ? rgb_mux[rgb_addr] : 8'b0;
 	
@@ -68,7 +69,7 @@ module vga_driver(
 	I_inst (
 		.hcnt(hcnt), 
 		.vcnt(vcnt), 
-		.enable(1'b1), 
+		.enable(enable[0]), 
 		.rgb(rgb_mux[0]), 
 		.active(rgb_sel[0])
 	);
@@ -83,7 +84,7 @@ module vga_driver(
 	Heart_inst (
 		.hcnt(hcnt), 
 		.vcnt(vcnt), 
-		.enable(1'b1), 
+		.enable(enable[1]), 
 		.rgb(rgb_mux[1]), 
 		.active(rgb_sel[1])
 	);
@@ -98,7 +99,7 @@ module vga_driver(
 	Katrina_inst (
 		.hcnt(hcnt), 
 		.vcnt(vcnt), 
-		.enable(1'b1), 
+		.enable(enable[2]), 
 		.rgb(rgb_mux[2]), 
 		.active(rgb_sel[2])
 	);
@@ -113,7 +114,7 @@ module vga_driver(
 	line1_inst (
 		.hcnt(hcnt), 
 		.vcnt(vcnt), 
-		.enable(1'b1), 
+		.enable(enable[3]), 
 		.rgb(rgb_mux[3]), 
 		.active(rgb_sel[3])
 	);
@@ -128,7 +129,7 @@ module vga_driver(
 	line2_inst (
 		.hcnt(hcnt), 
 		.vcnt(vcnt), 
-		.enable(1'b1), 
+		.enable(enable[4]), 
 		.rgb(rgb_mux[4]), 
 		.active(rgb_sel[4])
 	);
@@ -143,7 +144,7 @@ module vga_driver(
 	line3_inst (
 		.hcnt(hcnt), 
 		.vcnt(vcnt), 
-		.enable(1'b1), 
+		.enable(enable[5]), 
 		.rgb(rgb_mux[5]), 
 		.active(rgb_sel[5])
 	);
@@ -158,10 +159,54 @@ module vga_driver(
 	line4_inst (
 		.hcnt(hcnt), 
 		.vcnt(vcnt), 
-		.enable(1'b1), 
+		.enable(enable[6]), 
 		.rgb(rgb_mux[6]), 
 		.active(rgb_sel[6])
 	);
+
+	localparam [3:0] start = 0,
+			 s_i = 1,
+			 s_heart = 2,
+			 s_katrina = 3,
+			 s_msg = 4;
+
+	reg s_reg, s_next;
+	wire clk_en = 1'b1;
+
+	always @(posedge clk, posedge reset)
+		if (reset)
+			s_reg <= start;
+		else if (clk_en)
+			s_ref <= s_next;
+
+	always @(s_reg)
+	begin
+		//defaults
+		s_next = s_reg;
+		enable = 0;
+		case (s_reg)
+			start:
+				s_next = s_i;	
+			s_i:
+			begin
+				enable[0] = 1'b1;
+				s_next = s_heart;
+			end
+			s_heart:
+			begin
+				enable[1:0] = 2'b11;
+				s_next = s_katrina;
+			end
+			s_katrina:
+			begin
+				enable[2:0] = 3'b11;
+				s_next = s_msg;
+			end
+			s_msg:
+				enable[6:0] = 7'b1111111;
+		endcase
+	end
+
 	
 	function [31:0] log2;
 		input reg [31:0] value;
